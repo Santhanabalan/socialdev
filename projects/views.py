43b0e4ddd39d507bda00b3d28a1ab663de2e45bf
Santhanabalan <bala2102002@gmail.com>
2022-09-30 23:21:38 +0530

    Add CRUD Operations

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from projects.models import Project
+from projects.forms import ProjectForm
 
 # Create your views here.
 def projects(request):
@@ -12,3 +13,32 @@ def project(request,pk):
     tags = projectObj.tags.all()
     context = {'project':projectObj,'tags':tags}
     return render(request,'projects/single-project.html',context)
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('projects')
+    context={'form':form}
+    return render(request,'projects/project-form.html',context)
+
+def updateProject(request,pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect ('projects')
+    context={'form':form}
+    return render(request,'projects/project-form.html',context)
+
+def deleteProject(request,pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect ('projects')
+    context={'object':project}
+    return render(request,'projects/delete-object.html',context)
