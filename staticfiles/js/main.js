afcd4481b098998fc5847d466eea916bc3a857f8
santhanabalan-gitkraken <bala2102002@gmail.com>
2022-12-04 13:06:43 +0530

    Collectstatic

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,34 +1,23 @@
+
+//GET SEARCH FORM AND PAGE LINKS
 let searchForm = document.getElementById('searchForm')
-let pagelink = document.getElementsByClassName('page-link')
+let pageLinks = document.getElementsByClassName('page-link')
 
-if(searchForm){
-  for(let i=0;pagelink.length>i;i++){
-    pagelink[i].addEventListener('click' ,function (e){
-      e.preventDefault()
-      let page = this.dataset.page
-      searchForm.innerHTML += `<input value=${page} name="page">`
-      searchForm.submit()
-    })
-  }
-}
+//ENSURE SEARCH FORM EXISTS
+if (searchForm) {
+    for (let i = 0; pageLinks.length > i; i++) {
+        pageLinks[i].addEventListener('click', function (e) {
+            e.preventDefault()
 
-let tags = document.getElementsByClassName('project-tag')
-for (let i =0; tags.length > i; i++){
-    tags[i].addEventListener('click',(e)=>{
-        let tagId = e.target.dataset.tag
-        let projectId = e.target.dataset.project
+            //GET THE DATA ATTRIBUTE
+            let page = this.dataset.page
 
-        fetch('http://localhost:8000/api/remove-tag/',{
-            method: 'DELETE',
-            headers:{
-                'Content-Type':'application/json',
-            },
-            body:JSON.stringify({'project':projectId,'tag':tagId})
+            //ADD HIDDEN SEARCH INPUT TO FORM
+            searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+
+
+            //SUBMIT FORM
+            searchForm.submit()
         })
-        .then(response => response.json())
-        .then(data => {
-            e.target.remove()
-        })
-    })
-
+    }
 }
